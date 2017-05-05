0e64f151aa576e97b37ff0c27375e383142131a0
sanyoca <sanyoca@gmail.com>
2017-05-05 12:14:13 +0200

    V 1.0

--- app/src/main/java/com/example/sanya/goodread/MainActivity.java
+++ app/src/main/java/com/example/sanya/goodread/MainActivity.java
@@ -4,13 +4,17 @@ import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<BookDatas>>{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +42,22 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void startSearch(String criteria)    {
+        BookLoader loadBooks = new BookLoader(this, criteria);
+
+    }
+
+    @Override
+    public Loader<List<BookDatas>> onCreateLoader(int id, Bundle args) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<BookDatas>> loader, List<BookDatas> data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<BookDatas>> loader) {
 
     }
 }
